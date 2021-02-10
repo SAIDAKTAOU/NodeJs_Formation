54ded0a6cf29b44c7f760f45261940358bfb1c86
SAIDAKTAOU <s.aktaou@gmail.com>
2021-02-10 11:36:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars ({
+    layoutsDir:__dirname + '/views/layouts',
+}))
+
+app.get('/', function(req, res){
+    res.render('main', {layout : 'index'})
+});
+
+app.use(express.static('public'))
+
+app.listen(3000, function (){
+    console.log('Votre app est disponible sur localhost:3000 !');
+})
